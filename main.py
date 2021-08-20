1d83a26db170b8b693e5ac16c7baafe462cd2c1a
Chunibyo <chunibyo.wly@gmail.com>
2021-08-20 09:01:02 +0800

    wrap to class

--- main.py
+++ main.py
@@ -5,16 +5,33 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as expected
 from selenium.webdriver.support.wait import WebDriverWait
 
+
+class yqapp:
+    def __init__(self):
+        # 第三方登录页面
+        self.login_url = r"http://rsfw.cug.edu.cn/amp-auth-adapter/login?service=http://yqapp.cug.edu.cn/xsfw/sys/swmxsyqxxsjapp/*default/index.do"
+
+        # 创建chrome启动选项
+        self.chrome_options = webdriver.ChromeOptions()
+        # 指定chrome启动类型为headless 并且禁用gpu
+        self.chrome_options.add_argument('--headless')
+        self.chrome_options.add_argument('--disable-gpu')
+        self.driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=self.chrome_options)
+
+    def login(self):
+        wait = WebDriverWait(self.driver, timeout=10)
+        self.driver.get(self.login_url)
+
+        print(self.driver.get_cookies())
+
+    def close(self):
+        self.driver.quit()
+
+
+def main():
+    yq = yqapp()
+    yq.login()
+
+
 if __name__ == "__main__":
-    # 创建chrome启动选项
-    chrome_options = webdriver.ChromeOptions()
-
-    # 指定chrome启动类型为headless 并且禁用gpu
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=chrome_options)
-
-    wait = WebDriverWait(driver, timeout=10)
-    driver.get(
-        r'http://rsfw.cug.edu.cn/amp-auth-adapter/login?service=http://yqapp.cug.edu.cn/xsfw/sys/swmxsyqxxsjapp/*default/index.do')
-    driver.quit()
+    main()
